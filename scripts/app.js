dcb159cb03afdb2af86ccc07ff55fdd088c69092
ArcherHuang <mmosconii@gmail.com>
2018-06-12 22:30:33 +0800

    update code

--- scripts/app.js
+++ scripts/app.js
@@ -77,7 +77,7 @@ if (navigator.mediaDevices.getUserMedia) {
       soundClips.appendChild(clipContainer);
 
       audio.controls = true;
-      var blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+      var blob = new Blob(chunks, { 'type' : 'audio/wav' });
       chunks = [];
       var audioURL = window.URL.createObjectURL(blob);
       audio.src = audioURL;
